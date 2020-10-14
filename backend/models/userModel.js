67cba0a70d5d1aa60073d3b945a2efbd196713bc
Premraj01 <premrajtripute777@gmail.com>
2020-10-14 22:18:46 +0530

    BackEnd user auth commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,18 +1,16 @@
 /** @format */
 
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
-    name: {
-      fname: {
-        type: String,
-        required: true,
-      },
-      lname: {
-        type: String,
-        required: true,
-      },
+    fname: {
+      type: String,
+      required: true,
+    },
+    lname: {
+      type: String,
     },
 
     email: {
@@ -36,6 +34,19 @@ const userSchema = mongoose.Schema(
     timestamp: true,
   }
 )
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
 
